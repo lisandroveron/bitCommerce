a8245f251910a6cc4f752786ce3583890dc31bcd
lisandroveron <lisandro.veron@hotmail.com>
2022-06-17 16:11:05 -0300

    Added: First configuration and BitcoinPrice component.

---
+++ src/index.jsx
@@ -0,0 +1,30 @@
+// Packages
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Assets
+import { ReactComponent as BitcoinLogo } from "./assets/svg/bitcoin.svg";
+// Components
+import BitcoinPrice from "./components/BitcoinPrice.jsx";
+// CSS files
+import "./css/grid.css";
+import "./css/normalize.css";
+import "./css/index.css";
+
+
+// ---------- CODE ---------- //
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const App = <>
+	<header>
+		<div id="logo" className="logo">
+			<BitcoinLogo className="logo__B" />
+			<h1 className="logo__itCommerce">itCommerce</h1>
+		</div>
+	</header>
+	<main>
+		<BitcoinPrice />
+	</main>
+	<footer>Copyright © 2022 bitCommerce</footer>
+</>;
+
+root.render(App);
