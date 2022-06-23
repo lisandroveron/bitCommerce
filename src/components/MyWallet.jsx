70323955fcacf88cfe3d4ffb3f28f6107a145dc8
lisandroveron <lisandro.veron@hotmail.com>
2022-06-23 15:29:31 -0300

    Added: MyWallet component to know the current balance.

---
+++ src/components/MyWallet.jsx
@@ -0,0 +1,12 @@
+// CSS files
+import "./componentStyle/MyWallet.css";
+
+function MyWallet(){
+	return(<>
+		<div className="MyWallet">
+			<p><span>&#x0e3f;</span>1.70048</p>
+		</div>
+	</>);
+};
+
+export default MyWallet;
