2868109403d5f1367d37563ac58cde2c17cd98d8
lisandroveron <lisandro.veron@hotmail.com>
2022-07-06 20:17:36 -0300

    Added: props for Product component, now can be initialized with custom props. '.price' grid to display prices.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,22 @@
+// CSS files
 import "./componentStyle/Product.css";
 // Assets
-import rickgun from "../assets/img/rickgun.jpg";
+// import rickgun from "../assets/img/rickgun.jpg";
 
-function Product(){
+function Product(props){
 	return(<>
 		<div className="Product">
 			<div className="img">
-				<img src={rickgun} alt="Product." />
+				<img src={props.imgsrc} alt="Product." />
 			</div>
-			<p className="productName">Rick and Morty portal gun</p>
-			<input class="buy" type="button" value="Buy" />
+			<p className="productName">{props.productName}</p>
+			<div className="buy">
+				<input className="buyButton" type="button" value="Buy" />
+				<div className="prices">
+					<p className="prices__bitcoinPrice"><span>&#x0e3f;</span>0.1634</p>
+					<p className="prices__dollarPrice">$12.15</p>
+				</div>
+			</div>
 		</div>
 	</>);
 };
